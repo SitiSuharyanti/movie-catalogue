28d2eb9aeb2afaa707e3b4b6ed0ced0eb5f85731
SitiSuharyanti <siti.08@students.amikom.ac.id>
2022-12-03 00:27:07 +0700

    Melakukan revisi pada ci.yaml dan karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,5 @@
-// Karma configuration
-// Generated on Fri Jul 03 2020 20:15:52 GMT+0700 (Western Indonesia Time)
+// karma.conf.js
+
 module.exports = function (config) {
   config.set({
     /* Kode lainnya disembunyikan */
